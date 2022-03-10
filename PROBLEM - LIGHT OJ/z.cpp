6c38ccc9e286d8c8cbe88a7257c0468177522fdf
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2022-03-11 03:06:43 +0600

    1st commit

---
+++ PROBLEM - LIGHT OJ/z.cpp
@@ -0,0 +1,7 @@
+// Sum queries
+// Update queries
+// Finding the maximum and the number of times it appears
+// Compute the GCD / LCM
+// Counting the number of zeros, searching for the k-th zero
+// Find the smallest number greater or equal to a specified number. With modification queries.
+// Lazy propagation
